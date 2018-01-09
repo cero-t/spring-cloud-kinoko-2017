32570f33ac64734546ee77982263ef0415590931
Shin Tanimoto <cel@pos.to>
2018-01-10 00:49:33 +0900

    fix method name

--- services/store-web/src/main/java/ninja/cero/ecommerce/store/config/WebClientConfig.java
+++ services/store-web/src/main/java/ninja/cero/ecommerce/store/config/WebClientConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 	@Bean
-	WebClient restTemplate(LoadBalancerExchangeFilterFunction lbFunction) {
+	WebClient webClient(LoadBalancerExchangeFilterFunction lbFunction) {
 		return WebClient.builder().filter(lbFunction).build();
 	}
 }
