8ef9bd1d80e050de2c3202033165f382ddf06dfe
Shin Tanimoto <cero-t@users.noreply.github.com>
2018-04-16 23:07:57 +0900

    fix

--- services/stock/stock-service/src/main/java/ninja/cero/ecommerce/stock/config/JdbcConfig.java
+++ services/stock/stock-service/src/main/java/ninja/cero/ecommerce/stock/config/JdbcConfig.java
@@ -7,12 +7,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jdbc.core.DataAccessStrategy;
 import org.springframework.data.jdbc.core.DefaultDataAccessStrategy;
 import org.springframework.data.jdbc.core.SqlGeneratorSource;
+import org.springframework.data.jdbc.mapping.model.DelimiterNamingStrategy;
 import org.springframework.data.jdbc.mapping.model.JdbcMappingContext;
+import org.springframework.data.jdbc.mapping.model.NamingStrategy;
 
 @Configuration
 public class JdbcConfig {
 	@Bean
 	DataAccessStrategy dataAccessStrategy(JdbcMappingContext context, DataSource datasource) {
 		return new DefaultDataAccessStrategy(new SqlGeneratorSource(context), context);
+	}
+
+	@Bean
+	NamingStrategy namingStrategy() {
+		return new DelimiterNamingStrategy();
 	}
 }
