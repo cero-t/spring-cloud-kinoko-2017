d0cb6a169c1942353f56ef8697dabea9e9f2bf41
Shin Tanimoto <cero-t@users.noreply.github.com>
2018-04-16 23:07:45 +0900

    polish build process

--- ui/config/index.js
+++ ui/config/index.js
@@ -4,8 +4,10 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    // index: path.resolve(__dirname, '../dist/index.html'),
+    // assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(__dirname, '../../services/store-web/src/main/resources/static/index.html'),
+    assetsRoot: path.resolve(__dirname, '../../services/store-web/src/main/resources/static'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
