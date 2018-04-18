bee8dfcd494d3b94f12f3146856b311b4e5d04b9
Shin Tanimoto <cero-t@users.noreply.github.com>
2018-04-18 23:57:15 +0900

    apply Elastic APM

---
+++ services/order/order-service/src/main/java/ninja/cero/ecommerce/order/config/ApmConfig.java
@@ -0,0 +1,28 @@
+package ninja.cero.ecommerce.order.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import co.elastic.apm.servlet.ApmFilter;
+import co.elastic.apm.spring.webmvc.ApmHandlerInterceptor;
+
+@Configuration
+public class ApmConfig implements WebMvcConfigurer {
+	@Bean
+	public FilterRegistrationBean<ApmFilter> someFilterRegistration() {
+		FilterRegistrationBean<ApmFilter> registration = new FilterRegistrationBean<>();
+		registration.setFilter(new ApmFilter());
+		registration.addUrlPatterns("/*");
+		registration.setName("apmFilter");
+		registration.setOrder(1);
+		return registration;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new ApmHandlerInterceptor());
+	}
+}
